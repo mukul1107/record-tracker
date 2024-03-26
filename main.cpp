39d91849ebfd8736f006225b3e4d7ff5dded0205
mukul1107 <mukulsingh3078@gmail.com>
2024-03-26 22:45:58 +0530

    Student Data is stored in `data.txt`✅

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "src/course.h"
+#include <cstdlib> 
 #include "src/student.h"
 using namespace std;
 
@@ -8,12 +9,26 @@ using namespace std;
 
 int main(){
 
-    Student s1(427, "Damon", "BCA");
-    Course c1(101,"BCA");
+    Student std[5];
 
+    for(int i =0; i < 5; i++){
+        string name, course;
+        int id = rand();
 
-    c1.get_grades();
+        cout << "Enter the name of Student "<< i+1 << endl;
+        cin >> name;
+        cout << "Enter the Course of Student "<< i+1 << endl;
+        cin >> course;
+        std[i].set_info(id, name, course);
+        
+    }
 
+    for (int i = 0; i < 5; i++)
+    {
+        std[i].get_info();
+        cout << endl;
+    }
+    
 
     return 0;
 }
--- src/student.h
+++ src/student.h
@@ -3,47 +3,87 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
+#include <windows.h>
 #include <iostream>
+#include <fstream>
 using namespace std;
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-class Student{
-    private:
-        string name, course;
-        int id;
+void txtRed()
+{
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+}
+void txtGreen()
+{
+    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+}
+void txtWhite()
+{
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN);
+}
 
-    public:
+class Student
+{
+private:
+    string name, course;
+    int id;
 
-        
-        Student(int stdId, string stdName, string cs){
-            id = stdId;
-            name = stdName;
-            course = cs;
+public:
+    Student()
+    {
+        ;
+    }
+    Student(int stdId, string stdName, string cs)
+    {
+        id = stdId;
+        name = stdName;
+        course = cs;
+    }
+    void get_info()
+    {
+        cout << "Student's Name is: ";
+        txtGreen();
+
+        cout << name << endl;
+        txtWhite();
+        cout << "Student's ID is: ";
+
+        txtGreen();
+        cout << id << endl;
+        cout << name;
+        txtWhite();
+        cout << " is registered in: " << course << "." << endl;
+    }
+
+    void set_info(int roll, string uName, string cour)
+    {
+        roll = id;
+        name = uName;
+        course = cour;
+
+        ofstream studentData;
+        studentData.open("data.txt", ios::app);
+
+        studentData << name <<" " << course << " " << id << endl;
+        studentData << "\n";
+    }
+
+    void get_academics()
+    {
+        ;
+    }
+
+    void delete_info(int studentId)
+    {
+        if (studentId != id)
+        {
+            cout << "Invalid ID\n";
         }
-        void get_info(){
-            cout << "Student's Name is: "<< name <<"." <<endl;
-            cout << "Student's ID is: "<< id <<"." <<endl;
-            cout << name<<" is registered in: "<< course <<"." <<endl;
+        else if (studentId == id)
+        {
+            id = 0;
         }
-
-        void set_info(){
-            cout << "Enter your Name: " << endl;
-            cin >> name;
-            cout << "Enter your Course: " << endl;
-            cin >> course;
-        }
-
-        void get_academics(){
-            ;
-        }
-
-        void delete_info(int studentId){
-            if (studentId != id){
-                cout << "Invalid ID\n";
-            }else if(studentId == id){
-                 id = 0;
-                 
-            }
-        }
+    }
 };
 
 #endif
