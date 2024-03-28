b3621a6434a36397c609e884ffadbc7d0edc2be3
mukul1107 <mukulsingh3078@gmail.com>
2024-03-28 21:15:52 +0530

    set_grades() and get_grades() are available now

--- src/course.h
+++ src/course.h
@@ -1,41 +1,94 @@
-// course.h
-
 #ifndef COURSE_H
 #define COURSE_H
 
 #include <iostream>
-#include "./student.h"
+#include <string>
 using namespace std;
 
-class Course{
-    int courseId, credits;
+class Course
+{
+private:
+    int credits;
     string courseName;
     float grades[5];
-    string subs[5] = {"C++", "DBMS", "Operating Systems","UI/UX","DSA"};
-    public:
+    string subs[5] = {"C++", "DBMS", "Operating Systems", "UI/UX", "DSA"};
 
-        Course(int id, string name){
-            courseId = id;
-            courseName = name;
+public:
+    // Constructor
+    Course(string name)
+    {
+        courseName = name;
+    }
+
+    // Accessor methods
+    string getName() const
+    {
+        return courseName;
+    }
+
+    int getCredits() const
+    {
+        return credits;
+    }
+
+    // Method to check if a course is available in the syllabus
+    void checkCourseAvailability() const
+    {
+        string courseID;
+        bool isAvailable = false;
+        cout << "Enter the course name: ";
+        cin >> courseID;
+        for (int i = 0; i < 5; i++)
+        {
+            if (courseID == subs[i])
+            {
+                isAvailable = true;
+                break;
+            }
         }
 
-        void get_courseId(){
-            int userId;
-            cout << "Enter the course name :" << endl;
-            cin >> userId;
-            if (userId == courseId){
-                cout << "Course name is: " << courseName; 
-            }
-        };
-        void get_totalGPA();
-        void get_grades(){
-            cout << "Please enter the marks of specified subjects below." << endl;
-            for(int i =0; i < 5; i++){
-                cout << "Enter grades of " << subs[i] << endl;
-                cin >> grades[i];
-            }
+        if (isAvailable)
+        {
+            cout << courseID << " is Available in the Syllabus." << endl;
         }
-        void set_credits();
+        else
+        {
+            cout << "Unknown Course!" << endl;
+        }
+    }
+
+    // Method to get grades of specified subjects
+    void set_grades()
+    {
+        cout << "Please enter the marks of specified subjects below." << endl;
+        for (int i = 0; i < 5; i++)
+        {
+            cout << "Enter grades of " << subs[i] << ": ";
+            cin >> grades[i];
+        }
+    }
+
+    void get_grades(){
+        
+    }
+
+    // Method to set credits for the course
+    void set_credits(int credit)
+    {
+        credits = credit;
+    }
+
+    // Method to calculate total GPA for the course
+    void get_totalGPA() const
+    {
+        float totalGrade = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            totalGrade += grades[i];
+        }
+        float averageGrade = totalGrade / 5;
+        cout << "Total GPA for " << courseName << ": " << averageGrade << endl;
+    }
 };
 
 #endif
